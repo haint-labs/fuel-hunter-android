b81105aa1e0d85a2fe4b86037843e55fc5ca9b19
sashjakk <sashjakk@gmail.com>
2019-08-10 20:18:04 +0300

    Animate notification dismiss click

--- app/src/main/java/fuel/hunter/view/notification/NotificationView.kt
+++ app/src/main/java/fuel/hunter/view/notification/NotificationView.kt
@@ -1,8 +1,12 @@
 package fuel.hunter.view.notification
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.view.animation.AccelerateInterpolator
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.animation.doOnEnd
 import fuel.hunter.R
 
 class NotificationView @JvmOverloads constructor(
@@ -12,5 +16,26 @@ class NotificationView @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
     init {
         inflate(context, R.layout.layout_notification, this)
+
+        findViewById<View>(R.id.cancelIcon).setOnClickListener { dismiss() }
+    }
+
+    private fun dismiss() {
+        ValueAnimator
+            .ofInt(height, 0)
+            .apply {
+                duration = 300
+                interpolator = AccelerateInterpolator()
+
+                addUpdateListener {
+                    layoutParams = layoutParams.apply {
+                        height = it.animatedValue as Int
+                    }
+                    requestLayout()
+                }
+
+                doOnEnd { visibility = GONE }
+            }
+            .start()
     }
 }
