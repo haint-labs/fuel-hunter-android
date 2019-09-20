12af4a4dd7f0efc34b677146547f28c7e753d13d
sashjakk <sashjakk@gmail.com>
2019-09-20 17:29:29 +0300

    Allow to redefine color in shadow view

--- app/src/main/java/fuel/hunter/view/shadow/ShadowView.kt
+++ app/src/main/java/fuel/hunter/view/shadow/ShadowView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.annotation.IntDef
 import fuel.hunter.R
 import fuel.hunter.extensions.dp
 
@@ -13,14 +14,22 @@ class ShadowView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private val SINGLE = 0
-    private val TOP = 1
-    private val MIDDLE = 2
-    private val BOTTOM = 3
+    companion object {
+        const val SHADOW_SINGLE = 0
+        const val SHADOW_TOP = 1
+        const val SHADOW_MIDDLE = 2
+        const val SHADOW_BOTTOM = 3
 
-    var style = SINGLE
-    var color = Color.GRAY
+        @IntDef(SHADOW_SINGLE, SHADOW_TOP, SHADOW_MIDDLE, SHADOW_BOTTOM)
+        @Retention(AnnotationRetention.SOURCE)
+        annotation class ShadowStyle
+    }
+
     var cornerRadius = dp(4)
+
+    @ShadowStyle
+    var shadowStyle = SHADOW_SINGLE
+    var shadowColor = Color.GRAY
     var shadowRadius = cornerRadius
     var shadowAlpha = 255
 
@@ -28,26 +37,28 @@ class ShadowView @JvmOverloads constructor(
     var offsetBottom = dp(0)
 
     init {
-        val attributes = context
-            .theme
-            .obtainStyledAttributes(attrs, R.styleable.ShadowView, 0, 0)
+        val styledAttrs = context.theme.obtainStyledAttributes(
+            attrs, R.styleable.ShadowView, 0, 0
+        )
 
-        style = attributes.getInt(R.styleable.ShadowView_style, SINGLE)
-        color = attributes.getColor(R.styleable.ShadowView_shadowColor, color)
-        cornerRadius = attributes.getDimension(R.styleable.ShadowView_cornerRadius, cornerRadius)
-        shadowRadius = attributes.getDimension(R.styleable.ShadowView_shadowRadius, shadowRadius)
-        shadowAlpha = attributes.getInt(R.styleable.ShadowView_shadowAlpha, shadowAlpha).coerceIn(0, 255)
-        offsetTop = attributes.getDimension(R.styleable.ShadowView_offsetTop, offsetTop)
-        offsetBottom = attributes.getDimension(R.styleable.ShadowView_offsetBottom, offsetBottom)
+        with(styledAttrs) {
+            cornerRadius = getDimension(R.styleable.ShadowView_cornerRadius, cornerRadius)
 
-        attributes.recycle()
+            shadowStyle = getInt(R.styleable.ShadowView_style, SHADOW_SINGLE)
+            shadowColor = getColor(R.styleable.ShadowView_shadowColor, shadowColor)
+            shadowRadius = getDimension(R.styleable.ShadowView_shadowRadius, shadowRadius)
+            shadowAlpha = getInt(R.styleable.ShadowView_shadowAlpha, shadowAlpha).coerceIn(0, 255)
+
+            offsetTop = getDimension(R.styleable.ShadowView_offsetTop, offsetTop)
+            offsetBottom = getDimension(R.styleable.ShadowView_offsetBottom, offsetBottom)
+
+            recycle()
+        }
     }
 
-    private val paint = Paint().also {
-        it.color = color
-        it.alpha = shadowAlpha
-        it.isAntiAlias = true
-        it.maskFilter = BlurMaskFilter(shadowRadius, BlurMaskFilter.Blur.OUTER)
+    private val paint = Paint().apply {
+        isAntiAlias = true
+        maskFilter = BlurMaskFilter(shadowRadius, BlurMaskFilter.Blur.OUTER)
     }
 
     private val path = Path()
@@ -55,26 +66,31 @@ class ShadowView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        paint.apply {
+            color = shadowColor
+            alpha = shadowAlpha
+        }
+
         val h = height.toFloat()
         val w = width.toFloat()
         val cR = cornerRadius
 
         path.reset()
 
-        when (style) {
-            TOP -> {
+        when (shadowStyle) {
+            SHADOW_TOP -> {
                 path.addRoundRect(cR, offsetTop + cR, w - cR, h, cR, cR, Path.Direction.CCW)
                 path.addRect(cR, offsetTop + cR, w - 2 * cR, h, Path.Direction.CCW)
                 path.addRect(cR, offsetTop + cR + cR, w - cR, h, Path.Direction.CCW)
             }
-            MIDDLE -> {
+            SHADOW_MIDDLE -> {
                 path.addRect(cR, 0f, w - cR, h, Path.Direction.CCW)
             }
-            BOTTOM -> {
+            SHADOW_BOTTOM -> {
                 path.addRoundRect(cR, cR, w - cR, h - cR - offsetBottom, cR, cR, Path.Direction.CCW)
                 path.addRect(cR, 0f, w - cR, h - 2 * cR - offsetBottom, Path.Direction.CCW)
             }
-            SINGLE -> {
+            SHADOW_SINGLE -> {
                 path.addRoundRect(cR, cR, w - cR, h - cR, cR, cR, Path.Direction.CCW)
                 path.addRect(cR, cR, w - 2 * cR, h - 2 * cR, Path.Direction.CCW)
             }
