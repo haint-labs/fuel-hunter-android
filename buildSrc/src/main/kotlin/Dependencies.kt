a1c8f39d98e98a323f99c09c5e860e9825f22fbb
sashjakk <sashjakk@gmail.com>
2020-09-22 13:04:04 +0300

    Update plugins & libraries

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,22 +3,22 @@
 const val junit = "junit:junit:4.12"
 
 object kt {
-    const val version = "1.3.61"
+    const val version = "1.4.0"
     const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
 }
 
 object androidx {
-    private const val version = "1.2.0"
+    private const val version = "1.3.1"
     private const val lifecycleVersion = "2.2.0"
 
     const val core = "androidx.core:core-ktx:${version}"
-    const val appCompat = "androidx.appcompat:appcompat:1.1.0"
+    const val appCompat = "androidx.appcompat:appcompat:1.2.0"
 
-    const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0"
     const val recyclerView = "androidx.recyclerview:recyclerview:1.1.0"
     const val legacy = "androidx.legacy:legacy-support-v4:1.0.0"
-    const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:2.2.1"
-    const val navigationUi = "androidx.navigation:navigation-ui-ktx:2.2.1"
+    const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:2.3.0"
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:2.3.0"
 
     const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:$lifecycleVersion"
     const val lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel:$lifecycleVersion"
@@ -30,15 +30,15 @@ object androidx {
 }
 
 object firebase {
-    const val crashlytics = "com.google.firebase:firebase-crashlytics:17.0.0-beta02"
+    const val crashlytics = "com.google.firebase:firebase-crashlytics:17.2.1"
 }
 
 object Build {
     object Plugins {
-        const val android = "com.android.tools.build:gradle:3.5.3"
+        const val android = "com.android.tools.build:gradle:4.0.1"
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${kt.version}"
         const val googleServices = "com.google.gms:google-services:4.3.3"
-        const val crashlytics = "com.google.firebase:firebase-crashlytics-gradle:2.0.0-beta03"
+        const val crashlytics = "com.google.firebase:firebase-crashlytics-gradle:2.2.1"
 
         object ID {
             const val androidApp = "com.android.application"
