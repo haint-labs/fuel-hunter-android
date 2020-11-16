a684ac5aeb7115c4d059e1f1a12018e816f30f51
sashjakk <sashjakk@gmail.com>
2020-11-16 23:21:04 +0200

    Show snackbar message if no items for location

--- app/src/main/java/fuel/hunter/scenes/prices/PricesFragment.kt
+++ app/src/main/java/fuel/hunter/scenes/prices/PricesFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import fuel.hunter.R
 import fuel.hunter.databinding.FragmentPricesBinding
 import fuel.hunter.databinding.LayoutNotesBinding
@@ -41,8 +42,14 @@ class PricesFragment : Fragment() {
         setupPriceList()
         handleSavingsTap()
 
-        viewModel.prices
-            .observe(viewLifecycleOwner, adapter::submitList)
+        viewModel.prices.observe(viewLifecycleOwner) {
+            if (it.isEmpty()) {
+                Snackbar
+                    .make(view, "No prices for your location", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+            adapter.submitList(it)
+        }
     }
 
     private fun setupToolbar() {
