d96a1dd0dcaf7f11eeffc6335600274c6adb1f23
sashjakk <sashjakk@gmail.com>
2020-11-21 00:25:15 +0200

    Add base layout

---
+++ app/src/main/java/fuel/hunter/scenes/base/BaseLayout.kt
@@ -0,0 +1,25 @@
+package fuel.hunter.scenes.base
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BaseLayout(
+    toolbar: @Composable ColumnScope.() -> Unit,
+    children: @Composable BoxScope.() -> Unit,
+) {
+    MaterialTheme {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            alignment = Alignment.TopStart,
+        ) {
+            Column {
+                toolbar()
+                Box(children = children)
+            }
+        }
+    }
+}
