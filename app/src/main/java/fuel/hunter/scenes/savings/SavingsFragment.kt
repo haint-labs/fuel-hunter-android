76f3d1789c6ac30b7b75af7493ba8370828c88f7
sashjakk <sashjakk@gmail.com>
2020-11-21 00:27:19 +0200

    Re implement full savings scene with compose :scream:

--- app/src/main/java/fuel/hunter/scenes/savings/SavingsFragment.kt
+++ app/src/main/java/fuel/hunter/scenes/savings/SavingsFragment.kt
@@ -4,29 +4,123 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.DensityAmbient
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.ui.tooling.preview.Preview
 import fuel.hunter.R
 import fuel.hunter.databinding.FragmentSavingsBinding
-import fuel.hunter.databinding.LayoutToolbarBinding
-import fuel.hunter.extensions.init
-import fuel.hunter.extensions.viewBinding
+import fuel.hunter.scenes.base.BaseLayout
+import fuel.hunter.scenes.base.GlowingToolbar
+import fuel.hunter.scenes.base.rememberToolbarState
 
 class SavingsFragment : Fragment() {
-    private val binding by viewBinding(FragmentSavingsBinding::bind)
-    private val toolbarBinding by viewBinding(LayoutToolbarBinding::bind)
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = FragmentSavingsBinding.inflate(inflater, container, false).root
+    ): View {
+        val view = FragmentSavingsBinding.inflate(inflater, container, false)
+        val navController = findNavController()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        with(toolbarBinding) {
-            toolbarTitle.setText(R.string.title_savings)
-            toolbar.setNavigationIcon(R.drawable.ic_back_arrow)
+        view.composeView.setContent {
+            SavingsScene(navController::navigateUp)
+        }
 
-            init(toolbar, toolbarShadow, binding.scrollView)
+        return view.root
+    }
+}
+
+private val items = listOf(
+    R.drawable.ic_bag to R.string.savings_bag_description,
+    R.drawable.ic_map to R.string.savings_map_description,
+    R.drawable.ic_bell to R.string.savings_bell_description,
+    R.drawable.ic_bell to R.string.savings_bell_description,
+    R.drawable.ic_star to R.string.savings_star_description,
+)
+
+@Preview
+@Composable
+fun SavingsScene(
+    onNavigationClick: () -> Unit = {},
+) {
+    val scrollState = rememberScrollState(0f)
+    val toolbarState = rememberToolbarState(
+        color = colorResource(id = R.color.colorPrimary),
+        maxAlpha = with(DensityAmbient.current) { 50.dp.toPx() }
+    )
+
+    toolbarState.alpha = scrollState.value
+
+    BaseLayout(
+        toolbar = {
+            GlowingToolbar(
+                screenTitle = stringResource(id = R.string.title_savings),
+                navigationIcon = vectorResource(id = R.drawable.ic_back_arrow),
+                toolbarState = toolbarState,
+                onNavClick = onNavigationClick,
+            )
+        },
+    ) {
+        ScrollableColumn(
+            scrollState = scrollState,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 17.dp)
+        ) {
+            items.forEachIndexed { index, (iconId, descriptionId) ->
+                Column {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Image(
+                            asset = vectorResource(id = iconId),
+                            modifier = Modifier
+                                .padding(end = 17.dp)
+                        )
+
+                        Text(
+                            text = stringResource(id = descriptionId),
+                            color = colorResource(id = R.color.colorPrimary),
+                            fontSize = 16.sp,
+                        )
+                    }
+
+                    if (index < items.lastIndex) {
+                        Spacer(modifier = Modifier.height(10.dp))
+                    }
+                }
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .padding(vertical = 15.dp)
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(color = colorResource(id = R.color.colorPrimary))
+            )
+
+            Text(
+                text = stringResource(id = R.string.savings_example),
+                color = colorResource(id = R.color.colorPrimary),
+                fontSize = 16.sp,
+                modifier = Modifier
+                    .padding(bottom = 10.dp)
+            )
         }
     }
 }
