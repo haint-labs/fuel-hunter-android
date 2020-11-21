4a550ce4c8ff55b0a70cc798dd298e21857aa2ef
sashjakk <sashjakk@gmail.com>
2020-11-22 01:49:06 +0200

    Define initial colors & styles

---
+++ app/src/main/java/fuel/hunter/ui/Colors.kt
@@ -0,0 +1,7 @@
+package fuel.hunter.ui
+
+import androidx.compose.ui.graphics.Color
+
+val ColorPrimary = Color(66, 93, 146)
+val ColorSeparator = Color(215, 221, 232)
+val ColorSwitchUnchecked = Color(170, 170, 170)
---
+++ app/src/main/java/fuel/hunter/ui/Styles.kt
@@ -0,0 +1,16 @@
+package fuel.hunter.ui
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+val ListItemTitleTextStyle = TextStyle(
+    color = ColorPrimary,
+    fontSize = 18.sp,
+)
+
+val ItemTextStyle = TextStyle(
+    color = ColorPrimary,
+    fontSize = 18.sp,
+    textAlign = TextAlign.Center
+)
