e22fc4146d08b54ccc18d980aa81f8299af32c02
sashjakk <sashjakk@gmail.com>
2020-11-22 01:50:33 +0200

    Fix clipping not drawing content

--- app/src/main/java/fuel/hunter/view/decorations/GlowModifier.kt
+++ app/src/main/java/fuel/hunter/view/decorations/GlowModifier.kt
@@ -5,105 +5,11 @@ import android.graphics.Paint
 import android.graphics.Region
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.drawBehind
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.geometry.RoundRect
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-
-sealed class GlowStyle {
-    object Single : GlowStyle()
-    object Top : GlowStyle()
-    object Middle : GlowStyle()
-    object Bottom : GlowStyle()
-}
-
-fun singleItem(size: Size, radius: Float) = Path().apply {
-    addRoundRect(
-        RoundRect(
-            left = radius,
-            top = radius,
-            right = size.width - radius,
-            bottom = size.height - radius,
-            cornerRadius = CornerRadius(radius, radius),
-        )
-    )
-
-    addRect(
-        Rect(
-            left = radius,
-            top = radius,
-            right = size.width - 2 * radius,
-            bottom = size.height - 2 * radius,
-        )
-    )
-}
-
-fun topItem(size: Size, radius: Float) = Path().apply {
-    addRoundRect(
-        RoundRect(
-            left = radius,
-            top = radius,
-            right = size.width - radius,
-            bottom = size.height,
-            cornerRadius = CornerRadius(radius, radius),
-        )
-    )
-
-    addRect(
-        Rect(
-            left = radius,
-            top = radius,
-            right = size.width - 2 * radius,
-            bottom = size.height,
-        )
-    )
-
-    addRect(
-        Rect(
-            left = radius,
-            top = 2 * radius,
-            right = size.width - radius,
-            bottom = size.height,
-        )
-    )
-}
-
-fun middleItem(size: Size, radius: Float) = Path().apply {
-    addRect(
-        Rect(
-            left = radius,
-            top = 0f,
-            right = size.width - radius,
-            bottom = size.height,
-        )
-    )
-}
-
-fun bottomItem(size: Size, radius: Float) = Path().apply {
-    addRoundRect(
-        RoundRect(
-            left = radius,
-            top = radius,
-            right = size.width - radius,
-            bottom = size.height - radius,
-            cornerRadius = CornerRadius(radius, radius),
-        )
-    )
-
-    addRect(
-        Rect(
-            left = radius,
-            top = 0f,
-            right = size.width - radius,
-            bottom = size.height - 2 * radius,
-        )
-    )
-}
 
 fun Modifier.glow(
     radius: Dp = 10.dp,
@@ -128,7 +34,11 @@ fun Modifier.glow(
                 maskFilter = BlurMaskFilter(radius.toPx(), BlurMaskFilter.Blur.NORMAL)
             }
 
-        it.nativeCanvas.clipPath(nativePath, Region.Op.DIFFERENCE)
-        it.nativeCanvas.drawPath(nativePath, outerBlurPaint)
+        with(it.nativeCanvas) {
+            save()
+            clipPath(nativePath, Region.Op.DIFFERENCE)
+            drawPath(nativePath, outerBlurPaint)
+            restore()
+        }
     }
 }
